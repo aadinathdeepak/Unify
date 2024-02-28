3698aaf17a5f8da88ec5378f499158724ea2e7a5
tims-exe <timonkjohn@gmail.com>
2024-02-28 16:09:37 +0530

    logo added

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -7,7 +7,7 @@ export function InputBox({ label, placeholder, onChange }) {
         type="text"
         onChange={onChange}
         placeholder={placeholder}
-        className="w-10/12 h-11 px-2 py-1 rounded-md border-slate-200 bg-inputbox"
+        className="w-10/12 h-11 px-2 py-1 rounded-2xl border-slate-200 bg-inputbox"
       />
       </div>
       
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,16 @@
 import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
 import { Button } from "../components/Button";
+import logo from ".././assets/logo.png";
 
 export function Login() {
   return (
     <div className="flex h-screen justify-center bg-background text-text">
       <div className="flex flex-col justify-center ">
-        <div className="bg-[#6D357A] rounded-xl w-96 h-max p-3 px-4">
+        <div className="flex items-center justify-center">
+          <img className="h-[90px] my-5" src={logo}/>
+        </div>
+        <div className="bg-[#6D357A] rounded-3xl w-96 h-max p-3 px-4">
           <div className="text-center">
             <Heading label={"Login"} />
           </div>
