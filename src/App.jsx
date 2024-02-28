57dfe34975212016baa621b3f8b140fde9bab1b3
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-02-28 14:45:05 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
 import './App.css'
-
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<Navigate to={"/"}/>}/>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
