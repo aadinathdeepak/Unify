294e38bb047670ded81447cd29361fb9f9110952
tims-exe <timonkjohn@gmail.com>
2024-02-28 22:23:32 +0530

    profile page

---
+++ src/components/ProfileInputBoxBig.jsx
@@ -0,0 +1,15 @@
+export function ProfileInputBoxBig({ label, placeholder, onChange }) {
+  return (
+    <div>
+      <div className="flex justify-center ">
+      <input
+        type="text"
+        onChange={onChange}
+        placeholder={placeholder}
+        className="w-80 h-20 ml-10 px-4 py-1 rounded-2xl border-slate-200 bg-inputbox2 my-[15px]"
+      />
+      </div>
+      
+    </div>
+  );
+}
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,83 @@
+import logo from ".././assets/logo.png";
+import back from ".././assets/Back.png";
+import profilepic from ".././assets/ProfilePic.png";
+import { ProfileInputBox } from "../components/ProfileInputBox";
+import { ProfileInputBoxBig } from "../components/ProfileInputBoxBig";
+
+export function Profile(){
+    return <div className="bg-background h-screen w-screen pt-4">
+        <div className="flex flex-wrap">
+            <div className="w-5/6">
+            <img className="h-[90px] ml-12 pt-4 justify-start" src={logo}/>
+            </div>
+            <img className="h-[60px] ml-4 mt-4 pt-4" src={back}/>
+        </div>
+        <div className="flex">
+            <div className="flex w-2/3 m-14 justify-center items-center flex-col">
+                <div className="flex">
+                    <div className="flex items-center">
+                        <p className="text-text text-xl">
+                            Name
+                        </p>
+                    </div>
+                    <div className="flex items-center">
+                        <ProfileInputBox/>
+                    </div>
+                </div>
+                <div className="flex flex-wrap">
+                    <div className="flex items-center">
+                        <p className="text-text text-xl">
+                            College
+                        </p>
+                    </div>
+                    <div className="flex items-center">
+                        <ProfileInputBox/>
+                    </div>
+                </div>
+                <div className="flex flex-wrap">
+                    <div className="flex items-center">
+                        <p className="text-text text-xl">
+                            Branch
+                        </p>
+                    </div>
+                    <div className="flex items-center">
+                        <ProfileInputBox/>
+                    </div>
+                </div>
+                <div className="flex flex-wrap">
+                    <div className="flex items-center">
+                        <p className="text-text text-xl">
+                            Contact
+                        </p>
+                    </div>
+                    <div className="flex items-center">
+                        <ProfileInputBox/>
+                    </div>
+                </div>
+                <div className="flex flex-wrap">
+                    <div className="flex items-center">
+                        <p className="text-text text-xl">
+                            Club Roles
+                        </p>
+                    </div>
+                    <div className="flex items-center">
+                        <ProfileInputBoxBig/>
+                    </div>
+                </div>
+                <div className="flex flex-wrap">
+                    <div className="flex items-center">
+                        <p className="text-text text-xl">
+                            Projects
+                        </p>
+                    </div>
+                    <div className="flex items-center">
+                        <ProfileInputBoxBig/>
+                    </div>
+                </div>
+            </div>
+            <div className="justify-start">
+                <img className="h-[150px] mt-20 " src={profilepic}/>
+            </div>
+        </div>
+    </div>
+}
