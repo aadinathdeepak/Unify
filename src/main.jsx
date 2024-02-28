60c289f71cb9e792b8e3b3da9d259c0aad00d05f
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-02-28 22:52:49 +0530

    mui side bar component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ChakraProvider>
